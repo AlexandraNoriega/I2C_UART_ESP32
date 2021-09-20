1007ce73d358876e1e810033174f7e58502e4e3f
Amy Noriega <82512309+AlexandraNoriega@users.noreply.github.com>
2021-09-20 11:13:24 -0500

    Add files via upload

---
+++ UART/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "string.h"
+#include "driver/gpio.h"
+
+
+#define TXD_PIN (GPIO_NUM_17)
+#define RXD_PIN (GPIO_NUM_16)
+#define SERIAL_SIZE_RX 130
+static void rx_task();
+
+void init() 
+{
+    const uart_config_t uart_config = {
+        .baud_rate = 9600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+
+    //Set UART configuration parameters (UART PORT NUMBER, PARAMETERS SETTINGS)
+    uart_param_config(UART_NUM_1, &uart_config);
+    //configure the physical GPIO pins (PORT NUMBER, TX, RX, RTS (Flow control Request to Send), CTS(Flow control Clear to Send)) 
+    //If you want to keep a currently allocated pin number for a specific signal, pass the macro UART_PIN_NO_CHANGE
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    // We won't use a buffer for sending data.
+    // (PORT, RX BUFFER SIZE, TX BUFFER SIZE, UART QUEU SIZE, UART event queue handle (out param) ENABLE, FLAGS FOR INTERRUPTION)
+    uart_driver_install(UART_NUM_1, SERIAL_SIZE_RX, 0, 0, NULL, 0);
+}
+
+static void rx_task()
+{
+    static const char *RX_TASK_TAG = "RX_TASK";
+    uint8_t* data = (uint8_t*) malloc(SERIAL_SIZE_RX+1);
+    while (1) {
+        const int rxBytes = uart_read_bytes(UART_NUM_1, data, SERIAL_SIZE_RX, 1000 / portTICK_RATE_MS);
+        if (rxBytes > 0) {
+            data[rxBytes] = 0;
+            ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
+            ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+        }
+    }
+    free(data);
+}
+
+
+void app_main(void)
+{
+    init();
+    printf("\n Start testing");
+    rx_task();
+}
